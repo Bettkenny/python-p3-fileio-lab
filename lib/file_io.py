88991acd4324f0558b4f59ecb522f4f79227a692
YOUR NAME <YOUR@EMAIL.com>
2023-12-05 07:37:36 +0300

    completelab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
-def write_file(file_name, file_content):
+def write_file(file_name = "swot.txt", file_content= "name"):
+    with open(f'{file_name}.text', 'w') as f:
+        f.write(file_content)
+
     pass
 
 def append_file(file_name, append_content):
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
     pass
 
 def read_file(file_name):
+    with open (f'{file_name}.txt', 'r') as f:
+        file_content = f.read()
+    return file_content
     pass
